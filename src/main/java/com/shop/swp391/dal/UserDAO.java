9b0c47c585242faf8b4117aa0948d04e3af8880e
duyvu <dndv2204@gmail.com>
2025-03-18 11:32:36 +0700

    Add methods to UserDAO for retrieving customers with filters and counting filtered customers; enhance customer management functionality

--- src/main/java/com/shop/swp391/dal/UserDAO.java
+++ src/main/java/com/shop/swp391/dal/UserDAO.java
@@ -41,6 +41,27 @@ public class UserDAO extends DBContext implements I_DAO<User> {
         return users;
     }
 
+    public List<User> findAllCustomer() {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM SWP391_FASHION_SHOP.user";
+        try {
+            connection = getConnection();
+            statement = connection.prepareStatement(sql);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                User user = getFromResultSet(resultSet);
+                if (user.getRoleId() == 4) {
+                    users.add(user);
+                }
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            closeResources();
+        }
+        return users;
+    }
+
     @Override
     public boolean update(User user) {
         String sql = "UPDATE `SWP391_FASHION_SHOP`.`user`\n"
@@ -90,17 +111,17 @@ public class UserDAO extends DBContext implements I_DAO<User> {
     @Override
     public int insert(User user) {
         String sql = "INSERT INTO `SWP391_FASHION_SHOP`.`user` ("
-            + "`UserName`, "
-            + "`Password`, "
-            + "`Email`, "
-            + "`FirstName`, "
-            + "`LastName`, "
-            + "`Dob`, "
-            + "`Sex`, "
-            + "`Role`, "
-            + "`Phone`, "
-            + "`IsActive`) "
-            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+                + "`UserName`, "
+                + "`Password`, "
+                + "`Email`, "
+                + "`FirstName`, "
+                + "`LastName`, "
+                + "`Dob`, "
+                + "`Sex`, "
+                + "`Role`, "
+                + "`Phone`, "
+                + "`IsActive`) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
         try {
             connection = getConnection();
             statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -114,13 +135,13 @@ public class UserDAO extends DBContext implements I_DAO<User> {
             statement.setInt(8, user.getRoleId());
             statement.setString(9, user.getPhone());
             statement.setBoolean(10, user.isActive());
-            
+
             int affectedRows = statement.executeUpdate();
-            
+
             if (affectedRows == 0) {
                 throw new SQLException("Creating account failed, no rows affected.");
             }
-            
+
             resultSet = statement.getGeneratedKeys();
             if (resultSet.next()) {
                 return resultSet.getInt(1);
@@ -208,6 +229,59 @@ public class UserDAO extends DBContext implements I_DAO<User> {
         }
         return users;
     }
+    
+    public List<User> findCustomerWithFilters( String sexFilter, String statusFilter,
+            String searchFilter, int page, int pageSize) {
+
+        List<User> users = new ArrayList<>();
+        StringBuilder sql = new StringBuilder("SELECT * FROM SWP391_FASHION_SHOP.user where Role = ?");
+        List<Object> params = new ArrayList<>();
+        params.add(GlobalConfig.ROLE_CUSTOMER);
+
+        if (sexFilter != null && !sexFilter.isEmpty()) {
+            sql.append(" AND Sex = ?");
+            params.add(sexFilter.equals("1"));
+        }
+
+        if (statusFilter != null && !statusFilter.isEmpty()) {
+            sql.append(" AND IsActive = ?");
+            params.add(statusFilter.equals("1"));
+        }
+
+        if (searchFilter != null && !searchFilter.trim().isEmpty()) {
+            sql.append(" AND Email LIKE ? OR Phone LIKE ? OR FirstName LIKE ? OR LastName LIKE ?");
+            String searchPatterns = "%" + searchFilter.trim() + "%";
+            params.add(searchPatterns);
+            params.add(searchPatterns);
+            params.add(searchPatterns);
+            params.add(searchPatterns);
+        }
+
+        sql.append(" ORDER BY UserID LIMIT ? OFFSET ?");
+        params.add(pageSize);
+        params.add((page - 1) * pageSize);
+
+        try {
+            connection = getConnection();
+            statement = connection.prepareStatement(sql.toString());
+
+            for (int i = 0; i < params.size(); i++) {
+                statement.setObject(i + 1, params.get(i));
+            }
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                users.add(getFromResultSet(resultSet));
+
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error finding filtered users: " + ex.getMessage());
+        } finally {
+            closeResources();
+        }
+        return users;
+    }
+    
+    
 
     public int getTotalFilteredAccounts(String roleFilter, String sexFilter, String statusFilter,
             String searchFilter) {
@@ -259,6 +333,53 @@ public class UserDAO extends DBContext implements I_DAO<User> {
         return 0;
     }
 
+    
+    public int getTotalFilteredCustomer(String sexFilter, String statusFilter,
+            String searchFilter) {
+        StringBuilder sql = new StringBuilder("SELECT COUNT(*) FROM SWP391_FASHION_SHOP.user where Role = ?");
+        List<Object> params = new ArrayList<>();
+        params.add(GlobalConfig.ROLE_CUSTOMER);
+
+        if (sexFilter != null && !sexFilter.isEmpty()) {
+            sql.append(" AND Sex = ?");
+            params.add(sexFilter.equals("1"));
+        }
+
+        if (statusFilter != null && !statusFilter.isEmpty()) {
+            sql.append(" AND IsActive = ?");
+            params.add(statusFilter.equals("1"));
+        }
+
+        if (searchFilter != null && !searchFilter.trim().isEmpty()) {
+            sql.append(" AND Email LIKE ? OR Phone LIKE ? OR FirstName LIKE ? OR LastName LIKE ?");
+            String searchPattern = "%" + searchFilter.trim() + "%";
+            params.add(searchPattern);
+            params.add(searchPattern);
+            params.add(searchPattern);
+            params.add(searchPattern);
+        }
+
+        try {
+            connection = getConnection();
+            statement = connection.prepareStatement(sql.toString());
+
+            for (int i = 0; i < params.size(); i++) {
+                statement.setObject(i + 1, params.get(i));
+            }
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            }
+        } catch (Exception ex) {
+            System.out.println("Error counting filtered users: " + ex.getMessage());
+        } finally {
+            closeResources();
+        }
+        return 0;
+    }
+    
+    
     public boolean deactivateAccount(int userID) {
         String sql = "UPDATE SWP391_FASHION_SHOP.user SET IsActive = 0 WHERE UserID = ?";
         try {
@@ -294,7 +415,7 @@ public class UserDAO extends DBContext implements I_DAO<User> {
         }
         return null;
     }
-    
+
     public User findByEmail(User account) {
         String sql = "SELECT * FROM SWP391_FASHION_SHOP.user WHERE Email = ?";
         try {
@@ -312,7 +433,6 @@ public class UserDAO extends DBContext implements I_DAO<User> {
         }
         return null;
     }
-    
 
     public boolean updatePassword(User account) {
         String sql = "UPDATE SWP391_FASHION_SHOP.user SET Password = ? WHERE Email = ? OR UserName = ?";
@@ -322,7 +442,7 @@ public class UserDAO extends DBContext implements I_DAO<User> {
             statement.setString(1, account.getPassword()); // Cần mã hóa mật khẩu trước khi lưu
             statement.setString(2, account.getEmail());
             statement.setString(3, account.getUsername());
-    
+
             int affectedRows = statement.executeUpdate();
             return affectedRows > 0;
         } catch (SQLException ex) {
@@ -332,7 +452,6 @@ public class UserDAO extends DBContext implements I_DAO<User> {
             closeResources();
         }
     }
-    
 
     @Override
     public User getFromResultSet(ResultSet rs) throws SQLException {
